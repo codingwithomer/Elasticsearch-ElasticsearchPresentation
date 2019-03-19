d5c3d23fb98ed6064c0ac2927f3afb64d0434605
omer.can <omer.can@demirorenteknoloji.com>
2019-03-19 11:00:44 +0300

    Added admin controller and admin layout

---
+++ WhatIsElasticSearch.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhatIsElasticSearch.BLL.Interfaces;
+
+namespace WhatIsElasticSearch.MvcWebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _productService.GetAll();
+
+            return View(model);
+        }
+    }
+}
